a2b39a6348ed2b904323a5289e9486b39e99225b
Dieg Luna <diegofcolunalopez@gmail.com>
2021-07-09 10:08:39 -0500

    Inheritance in JS

--- JS/ES6/index.js
+++ JS/ES6/index.js
@@ -2,3 +2,15 @@ var car = new Car("AW426", new Account("Calipso Valdes", "QWE132"));
 
 car.passenger = 4;
 car.printDataCar();
+
+console.log("-------------------");
+
+var uberX = new UberX(
+  "AW426",
+  new Account("Calipso Valdes", "QWE132"),
+  "Chevrolet",
+  "spark"
+);
+
+uberX.passenger = 4;
+uberX.printDataCar();
