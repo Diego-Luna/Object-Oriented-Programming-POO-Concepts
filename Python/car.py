1f6cb53fdda589442bfad5044edabefb500d114d
Dieg Luna <diegofcolunalopez@gmail.com>
2021-07-09 09:16:53 -0500

    constructor methods in Python

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,11 @@
+from account import Account
+
 class Car:
   id            = int
   licence       = str
   driver        = str
   passegenger   = str
+
+  def __init__(self, license, driver):
+      self.licence  = license
+      self.driver   = driver
--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,21 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
-  print("Hola mundo");
-  car = Car();
-  car.licence = "AMS123"
-  car.driver = "Diego Luna"
-  print(vars(car))
+  print("Hola Mundo")
 
-  car2 = Car();
-  car2.licence = "AMS123"
-  car2.driver = "Piper MC"
-  print(vars(car2))
+  car = Car("AMS234", Account("Luke Castellan", "LC876"))
+  print(vars(car))
+  print(vars(car.driver))
+
+# if __name__ == "__main__":
+#   print("Hola mundo");
+#   car = Car();
+#   car.licence = "AMS123"
+#   car.driver = "Diego Luna"
+#   print(vars(car))
+
+#   car2 = Car();
+#   car2.licence = "AMS123"
+#   car2.driver = "Piper MC"
+#   print(vars(car2))
